2c9102fef563d0f6a1621385c5ed63004761b2b6
Lucy <frenchline707@gmail.com>
2020-09-02 12:41:27 +0900

    Chpt01. 프로젝트 환경설정 > 라이브러리 살펴보기
    build.gradle 의 dependencies 블록에 추가된 라이브러리가 실제 받아졌는지 확인하고, 서로 서로 어떻게 의존된 라이브러리를 일괄적으로 땡겨오는지 확인해 봄.

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
-		System.out.println("============ 앱 구동 및 내장 톰캣 실행 =============");
 		SpringApplication.run(HelloSpringApplication.class, args);
-		System.out.println("============ 톰캣 실행 후 =============");
 	}
 
 }
