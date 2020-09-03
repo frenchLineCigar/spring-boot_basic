625ee331c2672385679c6369ffb6e6a1746cfa9d
Lucy <frenchline707@gmail.com>
2020-09-04 04:00:02 +0900

    Chp03 회원 관리 예제 - 백엔드 개발 > 회원 서비스 및 테스트 케이스 작성
    > 회원 서비스 : MemberService.java
    > 회원 서비스 테스트 케이스 (AssertJ, JUnit 이용) : MemoryMemberRepositoryTest.java
    
    ============================
    
    * 복잡한 비즈니스 로직이 아니라 단순한 예제로 스프링 생태계 전반적으로 어떻게 개발이 일어나고 동작하는지 알아봄
    
    * 비즈니스 요구사항
    > 데이터: 회원ID, 이름
    > 기능: 회원 등록, 조회
    
    * 특이 사항
    > 아직 DB가 선정되지 않았는데, 개발을 해야한다. (가상의 시나리오 상황)
    > 아직 데이터 저장소가 선정되지 않아서, 우선 인터페이스로 구현 클래스를 변경할 수 있도록 설계
    > 데이터 저장소는 RDB, NoSQL 등등 다양한 저장소를 고민 중인 상황으로 가정
    > 개발을 진행하기 위해서 초기 개발 단계에서는 구현체로 가벼운 메모리 기반의 데이터 저장소 사용
    
    ============================
    
    * 일반적인 웹 어플리케이션 계층 구조를 따름
    
    > controller: 웹 MVC의 컨트롤러 역할
    > domain: 비즈니스 도메인 객체, 예) 회원, 주문, 쿠폰 등등 주로 데이터베이스에 저장하고 관리되는 비즈니스 도메인 객체
    > service: 핵심 비즈니스 로직 구현, 비즈니스 도메인 객체를 가지고 핵심 비즈니스 로직이 동작하도록 구현한 계층
    > repository: 데이터베이스에 접근, 도메인 객체를 DB에 저장하고 관리
    
    * 클래스 의존 관계
    
    MemberService : 회원 비즈니스 로직
    MemberRepository(Interface) : 회원을 저장하는 것은 인터페이스로 설계 -> 이유 : 아직 DB가 선정되지 않았으므로 인터페이스를 만들고
    구현체를 각각 다른 기술로 만들어본다. 메모리에 데이터를 저장하는 것으로 시작. 향후에 DB 접근 기술을 선정할 때 단순 JDBC, MyBatis, JPA 등으로 저장 기술을 바꿔줄 수 있다는 가정 하에 설계한다. 바꿔 끼우기 위해 인터페이스를 정의해놓음

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,57 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by frenchline707@gmail.com on 2020-08-19
+ * Blog : http://frenchline707.tistory.com
+ * Github : http://github.com/frenchLineCigar
+ */
+
+/* 회원 도메인의 비즈니스 로직을 작성하는 계층 */
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) { //DI
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입 : 회원 객체를 받아 저장하고, 회원 id를 반환
+     */
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원 X (이름 -> 닉네임이라고 생각하자 )
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /* 중복 회원 검증 */
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()) //findByName()의 리턴 타입은 Optional<Member>
+                .ifPresent(m -> { //findByName() 리턴 값에 어떤 값(m)이 있으면(중복이 있으면), { 함수 } 가 동작한다
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * ID로 특정 회원 조회
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,92 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Created by frenchline707@gmail.com on 2020-08-19
+ * Blog : http://frenchline707.tistory.com
+ * Github : http://github.com/frenchLineCigar
+ */
+
+/* 회원 서비스 테스트 케이스 */
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    /* 각 Test 실행 전에 동작하도록 */
+    @BeforeEach //각 테스트 메서드 실행 전마다 어떤 동작을 하게끔 만드는 애노테이션
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    /* 각 Test가 끝낼 때마다 동작하도록 */
+    @AfterEach //각 테스트 메서드 실행이 끝날 때마다 어떤 동작을 하게끔 만드는 애노테이션 (콜백 메서드라고 보면 됨)
+    public void afterEach() {
+        memberRepository.clearStore(); //Repository를 비운다
+    }
+
+    @Test
+    void 회원가입() {
+        //given : 무언가가 주어졌는데 (이 데이터를 기반으로)
+        Member member = new Member();
+        member.setName("spring");
+
+        //when : 이것을 실행했을 때 (이걸 검증)
+        Long saveId = memberService.join(member); //저장한 id를 리턴
+
+        //then : 결과가 이게 나와야 돼 (검증부)
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    /* 테스트는 정상 플로우도 중요한데, 예외 플로우가 훨씬 더 중요함 */
+    @Test
+    public void 중복_회원_예외() { //중복 회원 검증 로직을 잘 타서 예외를 터트리는지 확인
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        //IllegalStateException e = assertThrows(NullPointerException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        //assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다22.");
+
+/*
+        //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+            fail("예외가 발생해야 합니다."); //예외를 못잡았으니 실패
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+            //assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.123123");
+        }
+*/
+
+        //then
+
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
